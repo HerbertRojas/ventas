8684f6cafadec25510cb55a81724bdf8fc67c52c
Herbert Rojas <herbertmarcusserojaschavez1996@gmail.com>
2024-02-17 15:06:40 -0500

    v03

--- Config/Config.php
+++ Config/Config.php
@@ -6,7 +6,7 @@
 // const db = "sistema";
 // const charset = "charset=utf8";
 
-const base_url = "http://localhost/ventas/";
+const base_url = "https://divas-2f21a3f507c0.herokuapp.com/ventas/";
 const host = "bivulozzj5cdkvqdp8ac-mysql.services.clever-cloud.com";
 const user = "uakr1xmnbwafi9cq";
 const pass = "lS0OToJJ1eZoHgxcLfwQ";
